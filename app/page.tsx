817a0c0d121ed2397a65a9a536b75a65e3f1e938
Randy <rccjoel@gmail.com>
2024-04-15 18:11:54 -0400

    Animation created in a separate file

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-'use client'
-
 import Navigation from "./ui/navigation"
-import { TypeAnimation } from 'react-type-animation'
+import Animation from "./ui/animation"
 import Footer from "./ui/footer"
 import Languages from "./ui/languages"
 
@@ -39,23 +37,7 @@ export default function Main() {
               Hello, I'm Randy.
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-            <TypeAnimation
-              sequence={[
-                // Same substring at the start will only be typed out once, initially
-                'We produce food for Mice',
-                1000, // wait 1s before replacing "Mice" with "Hamsters"
-                'We produce food for Hamsters',
-                1000,
-                'We produce food for Guinea Pigs',
-                1000,
-                'We produce food for Chinchillas',
-                1000
-              ]}
-              wrapper="span"
-              speed={50}
-              style={{ fontSize: '2em', display: 'inline-block' }}
-              repeat={Infinity}
-            />
+              <Animation />
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
