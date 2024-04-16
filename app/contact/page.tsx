3f3631cfdfd5b08f58333074e1c9159ea3802703
Randy <rccjoel@gmail.com>
2024-04-15 22:56:18 -0400

    final v1.2

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,7 +14,7 @@ export default function Contact() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-emerald-700">Deploy faster</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          <p className="mt-10 text-3xl font-bold tracking-tight text-white sm:text-4xl">
             Ready to Talk?
           </p>
           <p className="mt-6 text-lg leading-8 text-white">
